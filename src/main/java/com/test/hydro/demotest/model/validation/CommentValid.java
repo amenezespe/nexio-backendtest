dfaec2d938930758ed796fafb5db52cedceab2b7
tucamenezes <amenezespe@gmail.com>
2019-05-05 04:33:09 -0400

    ajustement dans la interface de validation

--- src/main/java/com/test/hydro/demotest/model/validation/CommentValid.java
+++ src/main/java/com/test/hydro/demotest/model/validation/CommentValid.java
@@ -2,6 +2,7 @@ package com.test.hydro.demotest.model.validation;
 
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,4 +12,10 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = CommentValidator.class)
 public @interface  CommentValid {
+
+    String message = "Le Commenraire n'est pas valide";
+
+    Class<?>[] group() default {};
+
+    Class<? extends Payload>[] payload() default {};
 }
